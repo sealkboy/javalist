bc950006d80069e19f9793916ac3b7aea1c30d8f
Seal Kevin Boy Abad <sealkevinboyabad@gmail.com>
2024-10-25 23:23:56 +0200

    Añadido método que retorna los días de la semana

--- src/main/java/javalist/JavaList.java
+++ src/main/java/javalist/JavaList.java
@@ -6,5 +6,10 @@ public class Semana {
 
     public void listaDiasSemana() {
         diasSemana = List.of("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
-}
+    }
 
+    public List<String> getDiasSemana(){
+        return new ArrayList<>(diasSemana);
+    }
+
+    
