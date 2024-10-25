e8ed41b733ce63279d3402ef0176fd92640d0fae
Seal Kevin Boy Abad <sealkevinboyabad@gmail.com>
2024-10-25 23:47:12 +0200

    Se crea clase de prueba SemanaTest y se añade método setup para inicializar la lista de días

---
+++ src/test/java/javalist/JavaListTest.java
@@ -0,0 +1,11 @@
+public class SemanaTest {
+    private Semana semana;
+
+    @BeforeEach
+    public void setUp() {
+        semana = new Semana();
+        semana.listaDiasSemana(); // Inicializar la lista de días
+    }
+
+    
+
